ad79bd7e43980477924226c529c6bee49c5bd5b7
Etay Reuven <etayruben@gmail.com>
2022-06-30 22:29:06 +0300

    removed an unsued component

--- frontend/src/components/chat.js
+++ frontend/src/components/chat.js
@@ -1,17 +1,28 @@
 import MessagesArea from "./messagesArea";
-import MessageInputField from "./messageInputField";
 import { useState } from "react";
 
 function Chat(props) {
   const [messages, setMessages] = useState([
-    { username: "Etay", message: "This is the first message" },
-    { username: "Nitsan", message: "This is the second message" },
+    {
+      username: "Etay",
+      message: "This is the first message",
+    },
+    {
+      username: "Nitsan",
+      message: "This is the second message",
+    },
   ]);
 
   return (
     <div className="chat">
       <MessagesArea messages={messages} username={props.username} />
-      <MessageInputField />
+      <div dir="auto" className="input-wrap">
+        <textarea
+          dir="auto"
+          placeholder="type a message here!"
+          className="text-area"
+        />
+      </div>
     </div>
   );
 }
