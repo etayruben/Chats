7ccc4d54d0643bfe295f7b467822a2b01c6b401a
Etay Reuven <etayruben@gmail.com>
2022-07-02 18:58:59 +0300

    added a start for the contacts

--- frontend/src/components/contacts.js
+++ frontend/src/components/contacts.js
@@ -1,7 +1,23 @@
-function Contacts() {
+import rooms from "../utils/rooms.json";
+
+function Contacts(props) {
   return (
     <div className="contacts">
-      <div style={{ height: "120%" }}></div>
+      {rooms.map((room) => (
+        <div className="contact-card-container">
+          <img
+            src="../images/background-image.jpg"
+            alt={room.roomName}
+            className="lobby-picture"
+          />
+          <div
+            onClick={(e) => props.handleContactClick(e, room.roomID)}
+            className="contact-card"
+          >
+            {room.roomName}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
