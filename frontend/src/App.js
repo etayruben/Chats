d2c7bc89f83ee4b8fd6d74b79a88dc680f6ef19e
Etay Reuven <etayruben@gmail.com>
2022-07-19 18:56:11 +0300

    starting a socket session only after login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,18 @@ import ChatsApp from "./components/chatsApp";
 import WelcomePage from "./components/welcome-page/welcomePage";
 import { io } from "socket.io-client";
 
-const socket = io.connect("ws://localhost:5000");
-
 function App() {
   const [userProperties, setUserProperties] = useState({
-    fullName: "Etay",
+    fullName: null,
     phoneNumber: null,
     token: null,
   });
 
-  if (userProperties.fullName)
+  if (userProperties.token) {
+    const socket = io.connect("ws://localhost:5000");
+
     return <ChatsApp userProperties={userProperties} socket={socket} />;
+  }
 
   return <WelcomePage setUserProperties={setUserProperties} />;
 }
